a003566d9a901700f0deb4ed28256028a9cc167c
Mosky117 <davi.mosky@outlook.com>
2023-05-30 18:20:49 +0200

    update document

--- assets/js/counter.js
+++ assets/js/counter.js
@@ -1,28 +1,53 @@
 let count=0;
-const plus=document.querySelector('#plus');
-const minus=document.querySelector('#minus');
-const reset_value=document.querySelector('#reset');
+const body=document.querySelector('.box')
+
+const number=document.createElement("p");
+number.innerText = "0";
+body.appendChild(number);
+number.className='number';
+
+const inside_box=document.createElement('div');
+body.appendChild(inside_box);
+inside_box.className='inside-box';
+
+const minus=document.createElement('button');
+minus.innerText='-';
+inside_box.appendChild(minus);
+minus.className='button';
+minus.id='reset';
+
+const reset=document.createElement('button');
+reset.innerText='Reset';
+inside_box.appendChild(reset);
+reset.className='button';
+reset.id='reset';
+
+const plus=document.createElement('button');
+plus.innerText='+';
+inside_box.appendChild(plus);
+plus.className='button';
+plus.id='plus';
 
 function add(){
     count+=1;
-    document.querySelector('.number').innerHTML=count;
+    number.innerHTML=count;
 }
 
 function subtract(){
-    if(document.querySelector('.number').innerHTML==0){
+    if(number.innerHTML==0){
         alert('Start adding some positive values');
     }
     else{
         count-=1;
-        document.querySelector('.number').innerHTML=count;
+        number.innerHTML=count;
     }
 }
 
-function reset(){
+function restart(){
     count=0;
-    document.querySelector('.number').innerHTML=count;
+    number.innerHTML=count;
 }
 
 plus.addEventListener('click',add);
 minus.addEventListener('click',subtract);
-reset_value.addEventListener('click',reset);
+reset.addEventListener('click',restart);
